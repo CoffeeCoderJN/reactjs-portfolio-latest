532d403273fcf8d9904dfb132690a17431edb4ad
Mukul <mukul.dharashivkar99@gmail.com>
2023-03-17 11:59:01 +0530

    Add framer motion to p in about

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,6 @@ import Section from "./common/Section";
 import MukulImg from "../assets/mukul.jpg";
 import { motion } from "framer-motion";
 
-
 const About = () => {
   return (
     <Section id="about" title="About Me" subtitle="Full Stack Developer">
@@ -21,7 +20,13 @@ const About = () => {
           viewport={{ once: true }}
           className="mb-20 md:mb-0 flex-shrink-0 rounded-full w-56 h-56 object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[300px] xl:h-[350px]"
         />
-        <div className="space-y-10 px-0 md:px-10 xl:pt-0 xl:px-20 md:pt-10">
+        <motion.div
+          initial={{ x: 200, opacity: 0 }}
+          transition={{ duration: 1.2 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          className="space-y-10 px-0 md:px-10 xl:pt-0 xl:px-20 md:pt-10"
+        >
           <h4 className="text-2xl font-light">Here's a Little Background.</h4>
           <p className="text-base font-extralight">
             Hi, I am Mukul Rajendra Dharashivkar and everyone calls me Mukul. I
@@ -38,7 +43,7 @@ const About = () => {
             and increase my skills in software development and web development.
             In my spare time I play cricket and read blogs.
           </p>
-        </div>
+        </motion.div>
       </motion.div>
     </Section>
   );
