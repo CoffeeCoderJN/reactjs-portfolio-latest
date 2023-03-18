b2810ceb16f59c85885759794eae67741caa1022
Mukul <mukul.dharashivkar99@gmail.com>
2023-03-18 16:37:59 +0530

    Change bg color

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="h-screen">
       <div className={darkMode && "dark"}>
-        <main className="bg-white dark:bg-[#050a14] dark:text-white">
+        <main className="bg-white dark:bg-gray-900 to-gray-900 dark:text-white">
         <Header darkMode={darkMode} setDarkMode={setDarkMode} />
 
           <motion.div
