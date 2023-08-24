4c2ab5b1d2cb00eb03c86e313a6b4465271e1d71
Mukul <mukul.dharashivkar99@gmail.com>
2023-08-24 18:35:11 +0530

    Changed CSS of all skills and projects

--- src/components/common/Section.js
+++ src/components/common/Section.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Section = ({id, title, subtitle, children}) => {
   return (
-    <section id={id} className="min-h-fit flex flex-col justify-start items-center py-16 px-5 text-center">
+    <section id={id} className="min-h-fit flex flex-col justify-start items-center py-16 px-2 md:px-5 text-center">
         <h3 className="py-3 text-5xl lg:text-5xl font-bold text-amber-500 uppercase tracking-[1px]">{title}</h3>
         <p className="max-w-xl font-semibold text-gray-400 mb-10 text-sm md:text-base">{subtitle}</p>
         {children}
