f79082193c3349f1dd0067bedf068f860d0f276b
Mukul Dharashivkar <mukul.dharashivkar99@gmail.com>
2023-10-11 21:43:20 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
       id="about"
       title="Overview"
       subtitle="Full Stack Developer"
-      className="relative pt-20"
+      className="relative"
     >
       <img
         src={RightSvg}
