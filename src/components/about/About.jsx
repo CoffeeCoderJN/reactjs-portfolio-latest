bf9b7cd3987b8c50eb5a1fa15e680edcc7967dc3
Mukul <mukul.dharashivkar99@gmail.com>
2023-10-11 19:15:35 +0530

    [ALL] update animations and added new hero marquee

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
       <img
         src={RightSvg}
         alt=""
-        className="absolute hidden right-0 bottom-2/3 w-2/12 max-w-xs md:block"
+        className="absolute hidden right-0 bottom-[40%] w-2/12 max-w-xs md:block"
         loading="lazy"
         height={700}
         width={320}
